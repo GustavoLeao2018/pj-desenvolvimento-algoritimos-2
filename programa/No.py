8d572cff44569311295c1605db1e56a8a8f88b33
GustavoLeao2018 <gustavo.leao.nogueira.2018@gmail.com>
2018-11-25 12:00:28 -0200

    Criado a classe nó com o método __init__ e __str__.

--- programa/No.py
+++ programa/No.py
@@ -2,3 +2,9 @@ class No:
     def __init__(self, valor):
         self.dado = valor
         self.proximo = None
+
+    def __str__(self):
+        if self.proximo is None:
+            return f"Nó: {self.dado} \t|\t Próximo: {self.proximo}"
+        else:
+            return f"Nó: {self.dado} \t|\t Próximo: {self.proximo.dado}"
